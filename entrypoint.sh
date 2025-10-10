7a6f433b458589ed2708df65211e010436dafe9c
Fabricio Nicolas Duarte <fabricioduarteoficial@gmail.com>
2025-10-10 00:14:46 -0300

    Edicion de archivo entrypoint.sh.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
-# Salir inmediatamente si un comando falla
+Salir inmediatamente si un comando falla
 set -e
 
-# Ejecutar las migraciones de la base de datos
+Crear el enlace simbólico para el storage (CRUCIAL para Render)
+echo "Running storage:link..."
+php artisan storage:link
+
+Ejecutar las migraciones de la base de datos
 echo "Running database migrations..."
 php artisan migrate --force
 
-# Iniciar el servidor de Laravel
+Iniciar el servidor de Laravel
 echo "Starting Laravel server..."
 exec php artisan serve --host=0.0.0.0 --port=10000
